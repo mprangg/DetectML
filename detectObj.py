ae8291255bba14758025e586843a843b43edcdd3
Tong <nonlapun_28@hotmail.com>
2018-02-08 00:53:02 +0700

    Update detectObj.py

--- detectObj.py
+++ detectObj.py
@@ -1,205 +1,92 @@
-import freenect
+from os import path
+import pyaudio
 import time
 
-import numpy as np
-import os
-import six.moves.urllib as urllib
-import manageDB as mdb
-import tarfile
-import tensorflow as tf
+from pocketsphinx.pocketsphinx import *
+from sphinxbase.sphinxbase import *
 
-from object_recognition_detection.utils import label_map_util
-from object_recognition_detection.utils import visualization_utils as vis_util
-
-# What model to download.
-MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017'
-MODEL_FILE = MODEL_NAME + '.tar.gz'
-DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
-
-# Path to frozen detection graph. This is the actual model that is used for the object detection.
-PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
-
-# List of the strings that is used to add correct label for each box.
-PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
-
-NUM_CLASSES = 90
+from check4 import *
+from manageDB import *
 
 
-# ## Download Model
+from espeak import espeak
+espeak.set_parameter(espeak.Parameter.Pitch, 60)
+espeak.set_parameter(espeak.Parameter.Rate, 110)
+espeak.set_parameter(espeak.Parameter.Range, 600)
+espeak.synth("Hey Guys My name is Jerry")
+time.sleep(2)
 
-if not os.path.exists(MODEL_NAME + '/frozen_inference_graph.pb'):
-	print ('Downloading the model')
-	opener = urllib.request.URLopener()
-	opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
-	tar_file = tarfile.open(MODEL_FILE)
-	for file in tar_file.getmembers():
-	  file_name = os.path.basename(file.name)
-	  if 'frozen_inference_graph.pb' in file_name:
-	    tar_file.extract(file, os.getcwd())
-	print ('Download complete')
-else:
-	print ('Model already exists')
+MODELDIR = "/home/uawsscu/PycharmProjects/DetectML/object_recognition_detection/model_LG"
+DATADIR = "/home/uawsscu/PycharmProjects/DetectML/object_recognition_detection/dataLG"
 
-# ## Load a (frozen) Tensorflow model into memory.
+config = Decoder.default_config()
+config.set_string('-logfn', '/dev/null')
+config.set_string('-hmm', path.join(MODELDIR, 'en-us/en-us'))
+config.set_string('-lm', path.join(MODELDIR, 'en-us/en-us.lm.bin'))
+config.set_string('-dict', path.join(MODELDIR, 'en-us/cmudict-en-us.dict'))
+decoder = Decoder(config)
 
-detection_graph = tf.Graph()
-with detection_graph.as_default():
-  od_graph_def = tf.GraphDef()
-  with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
-    serialized_graph = fid.read()
-    od_graph_def.ParseFromString(serialized_graph)
-    tf.import_graph_def(od_graph_def, name='')
+# Switch to JSGF grammar
+jsgf = Jsgf(path.join(DATADIR, 'sentence.gram'))
+rule = jsgf.get_rule('sentence.move') #>> public <move>
+fsg = jsgf.build_fsg(rule, decoder.get_logmath(), 7.5)
+fsg.writefile('sentence.fsg')
 
+decoder.set_fsg("sentence", fsg)
+decoder.set_search("sentence")
 
-# ## Loading label map
-# Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=1024)
+stream.start_stream()
 
-label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
-category_index = label_map_util.create_category_index(categories)
+in_speech_bf = False
+decoder.start_utt()
+while True:
 
-#intializing the web camera device
+    buf = stream.read(1024)
+    if buf:
+        decoder.process_raw(buf, False, False)
 
-import cv2
+        if decoder.get_in_speech() != in_speech_bf:
+            in_speech_bf = decoder.get_in_speech()
+            if not in_speech_bf:
+                decoder.end_utt()
 
-def get_video():
-    array, _ = freenect.sync_get_video()
-    array = cv2.cvtColor(array, cv2.COLOR_RGB2BGR)
-    return array
+                try:
+                    strDecode = decoder.hyp().hypstr
+                    if strDecode != '':
+                        print 'Stream decoding result:', strDecode
 
-
-# Running the tensorflow session
-def detect_Object():
-    tmp = ''
-    with detection_graph.as_default():
-        with tf.Session(graph=detection_graph) as sess:
-            ret = True
-            while (ret):
-                image_np = get_video()
-                check =""
-
-                image_np_expanded = np.expand_dims(image_np, axis=0)
-                image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-
-                boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-
-                scores = detection_graph.get_tensor_by_name('detection_scores:0')
-                classes = detection_graph.get_tensor_by_name('detection_classes:0')
-                num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-                # Actual detection.
-                (boxes, scores, classes, num_detections) = sess.run(
-                    [boxes, scores, classes, num_detections],
-                    feed_dict={image_tensor: image_np_expanded})
-
-                classType =vis_util.f.attr
-
-                if(classType != tmp and classType != 0 ):
-                    #print classType
-                    tmp = classType
-                    mdb.insert_Buff_Detect(classType)
+                        if strDecode[-3:] == 'end':
+                            obj_name = get_object_train(strDecode)
+                            buff = search_Buff_Detect(1)
+                            print "Search : ", search_Buff_Detect(1)
+                            if(obj_name == buff):
+                                print "ADD Object to DB"
+                                lenObj = int(lenDB("corpus_Obj.db", "SELECT * FROM object_Train")) # count ROWs
+                                obj_check = insert_object_Train(obj_name,lenObj+1) #check Found objects?
 
 
+                        elif strDecode[:5] == 'jerry':
+                            get_object_command(strDecode)
+                            #corpus_Arm
 
-                # Visualization of the results of a detection.
-                vis_util.visualize_boxes_and_labels_on_image_array("CHECK",
-                    image_np,
-                    np.squeeze(boxes),
-                    np.squeeze(classes).astype(np.int32),
-                    np.squeeze(scores),
-                    category_index,
-                    use_normalized_coordinates=True,
-                    line_thickness=8)
+                        elif strDecode[:11] == 'do you know':
+                            obj_name = get_object_question(strDecode)
+                            obj_find = search_object_Train(obj_name)
 
+                            if obj_find != "None":
+                                print "No, I don't know!"
+                            else: print "Yes , I know!"
 
-                cv2.imshow('image', cv2.resize(image_np, (640, 480)))
-
-                if cv2.waitKey(25) & 0xFF == ord('q'):
-                    cv2.destroyAllWindows()
-
-                    break
+                        elif strDecode[:14] == 'this is how to':
+                            get_TrainArm(strDecode)
 
 
-
-def detectRT():
-    with detection_graph.as_default():
-        with tf.Session(graph=detection_graph) as sess:
-            ret = True
-            while (ret):
-                image_np, _ = freenect.sync_get_video()
-                image_np = cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR)
-
-                image_np_expanded = np.expand_dims(image_np, axis=0)
-                image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-
-                boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-
-                scores = detection_graph.get_tensor_by_name('detection_scores:0')
-                classes = detection_graph.get_tensor_by_name('detection_classes:0')
-                num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-                # Actual detection.
-                (boxes, scores, classes, num_detections) = sess.run(
-                    [boxes, scores, classes, num_detections],
-                    feed_dict={image_tensor: image_np_expanded})
-
-                # Visualization of the results of a detection.
-                vis_util.visualize_boxes_and_labels_on_image_array("CHECK",
-                    image_np,
-                    np.squeeze(boxes),
-                    np.squeeze(classes).astype(np.int32),
-                    np.squeeze(scores),
-                    category_index,
-                    use_normalized_coordinates=True,
-                    line_thickness=8)
-
-                cv2.imshow('image', cv2.resize(image_np, (640, 480)))
-
-                if cv2.waitKey(25) & 0xFF == ord('q'):
-                    cv2.destroyAllWindows()
-
-                    break
-
-
-def detect_Webcam() :
-    cap = cv2.VideoCapture(0)
-
-    # Running the tensorflow session
-    with detection_graph.as_default():
-        with tf.Session(graph=detection_graph) as sess:
-            ret = True
-            while (ret):
-                ret, image_np = cap.read()
-                # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-                image_np_expanded = np.expand_dims(image_np, axis=0)
-                image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-                # Each box represents a part of the image where a particular object was detected.
-                boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-                # Each score represent how level of confidence for each of the objects.
-                # Score is shown on the result image, together with the class label.
-                scores = detection_graph.get_tensor_by_name('detection_scores:0')
-                classes = detection_graph.get_tensor_by_name('detection_classes:0')
-                num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-                # Actual detection.
-                (boxes, scores, classes, num_detections) = sess.run(
-                    [boxes, scores, classes, num_detections],
-                    feed_dict={image_tensor: image_np_expanded})
-                # print classes
-                # Visualization of the results of a detection.
-                vis_util.visualize_boxes_and_labels_on_image_array("CHECK",
-                                                                   image_np,
-                                                                   np.squeeze(boxes),
-                                                                   np.squeeze(classes).astype(np.int32),
-                                                                   np.squeeze(scores),
-                                                                   category_index,
-                                                                   use_normalized_coordinates=True,
-                                                                   line_thickness=8)
-                #      plt.figure(figsize=IMAGE_SIZE)
-                #      plt.imshow(image_np)
-                cv2.imshow('image', cv2.resize(image_np, (1280, 960)))
-                if cv2.waitKey(25) & 0xFF == ord('q'):
-                    cv2.destroyAllWindows()
-                    cap.release()
-                    break
-
-#detect_Webcam()
-detect_Object()
-#detectRT()
+                except AttributeError:
+                    pass
+                decoder.start_utt()
+    else:
+        break
+decoder.end_utt()
+print('An Error occured :', decoder.hyp().hypstr)
