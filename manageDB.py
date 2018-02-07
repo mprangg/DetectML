b22eb80734f627e3c1dcb48fa30537de9ba9862e
Tong <nonlapun_28@hotmail.com>
2018-02-07 13:41:37 +0700

    Update manageDB.py

--- manageDB.py
+++ manageDB.py
@@ -28,13 +28,41 @@ def lenDB(nameDB,exeText):
 def search_object_Train(name):
     with sqlite3.connect("corpus_Obj.db") as con:
         cur = con.cursor()
-        cur.execute('SELECT * FROM object_Train WHERE name=?',(name,))
-        return cur.fetchone() # None OR (u'Ball', 2)
+        #cur.execute('SELECT * FROM object_Train WHERE name=?',(name,))
+        cur.execute("SELECT " + "ID" + " FROM object_Train where " + "name" + "=?", (name,))
+        rows = cur.fetchone()
+        for element in rows:
+            return element
 
+       # return cur.fetchone() # None OR (u'Ball', 2)
+
+#>>>>>>>>>>>>>>>>JSON<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+import pyodbc
+import json
+import collections
+
+def Jtest():
+    connection = sqlite3.connect('corpus_Obj.db')
+    cursor = connection.cursor()
+    query = "SELECT * FROM object_Train"
+    result = cursor.execute(query)
+
+    items2 = [dict(zip([key[0] for key in cursor.description], row)) for row in result]
+    print (json.dumps({'items2': items2}))
+
+    with open('JsonObj1.json', 'w') as outfile:
+        json.dump(items2, outfile)
+    # Convert query to objects of key-value pairs
+"""
 if __name__ == '__main__':
     #create_1()
     lenObj = int(lenDB("corpus_Obj.db","SELECT * FROM object_Train"))
-    print search_object_Train("all")
+    #print search_object_Train("all")
     A= "ball"
     #print lenObj
     #print insertObj_NameNum(A,lenObj)   #/// DUPLICATE or NONE
+
+print search_object_Train("Ball")
+
+"""
+
