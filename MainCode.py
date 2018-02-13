99e2cae01684e311b6eaf7a5cdff044f4edab8db
Tong <nonlapun_28@hotmail.com>
2018-02-14 01:01:29 +0700

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -41,6 +41,9 @@ stream.start_stream()
 
 in_speech_bf = False
 decoder.start_utt()
+
+STPindex = 0
+STPname =""
 while True:
 
     buf = stream.read(1024)
@@ -56,31 +59,51 @@ while True:
                     strDecode = decoder.hyp().hypstr
                     if strDecode != '':
                         print 'Stream decoding result:', strDecode
-
+                        # >>>>>>> END <<<<<<<<<<<<
                         if strDecode[-3:] == 'end':
                             obj_name = get_object_train(strDecode)
                             buff = search_Buff_Detect(1)
+                            try :
+                                remove_Buff_Detect(1)
+                            except :
+                                print "Table Buffer NULL"
+
                             print "Search : ", search_Buff_Detect(1)
+
                             if(obj_name == buff):
-                                print "ADD Object to DB"
+                                print "ADD Object to Table object_Train : ",obj_name
                                 lenObj = int(lenDB("corpus_Obj.db", "SELECT * FROM object_Train")) # count ROWs
                                 obj_check = insert_object_Train(obj_name,lenObj+1) #check Found objects?
 
+                        # >>>>>>> ARM <<<<<<<<<<<<
+                        elif STPindex == 0 and strDecode[:14] == 'this is how to' and strDecode[-4:] == "step":
 
+                            STPindex = int(text2int(strDecode))
+                            STPname = get_TrainArm(strDecode)  # grab ball
+                            print("SAVE NAME TO Table Main_action")
+
+
+                        elif STPindex > 0 and strDecode == 'call back step':
+
+                            STPindex -= 1
+                            print STPindex, " : ", STPname
+
+                        # >>>>>>> JERRY <<<<<<<<<<<<
                         elif strDecode[:5] == 'jerry':
-                            get_object_command(strDecode)
+                            print get_object_command(strDecode)
                             #corpus_Arm
-
+                        
+                        
+                        # >>>>>>> PASS DO YOU KNOW~??? <<<<<<<<<<<<
                         elif strDecode[:11] == 'do you know':
                             obj_name = get_object_question(strDecode)
+                            print(obj_name)
                             obj_find = search_object_Train(obj_name)
 
                             if obj_find != "None":
-                                print "No, I don't know!"
-                            else: print "Yes , I know!"
+                                print "Yes , I know!"
+                            else: print "No , I don't know!"
 
-                        elif strDecode[:14] == 'this is how to':
-                            get_TrainArm(strDecode)
 
 
                 except AttributeError:
